3954c40dde236b9e4a89469ae05d912a0e57e943
Wei-Xiang Lin <118544555+LemonSNS@users.noreply.github.com>
2024-02-06 09:20:23 +0000

    basic actions and dmg numbers done. actionmanager half done.

--- Assets/Scripts/management/actionManager.cs
+++ Assets/Scripts/management/actionManager.cs
@@ -4,19 +4,49 @@ using UnityEngine;
 
 public class actionManager : MonoBehaviour
 {
-    public List<Action> actionQueue = new List<Action>(); // I call it a queue, but it's not the CS textbook definition of a queue. 
+    private List<Action> actionQueue = new List<Action>(); // I call it a queue, but it's not the CS textbook definition of a queue. 
     public Action currentAction;
+    public static actionManager Instance;
+    public static event Action<actionType> preCardInvoke;
+    public static event Action<actionType> postCardInvoke;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Awake(){
+        if (Instance != null){
+            Debug.Log("More than one actionManager active. What did you do??? Continued playing may break the game.");
+        }
+        Instance = this;
+
+        currentAction = new Action();
+        //an empty action is produced to avoid null errors
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (actionQueue.Count != 0 && currentAction.isDone){
-            //code goes here.
+            currentAction = nextAction();
         }
+        else if (!currentAction.isDone){
+            preCardInvoke?.Invoke(currentAction.actionType);
+            currentAction.act();
+            postCardInvoke?.Invoke(currentAction.actionType);
+        }
+    }
+
+    public void addToBottom(List<Action> actionList){
+        actionQueue.InsertRange(0, actionList);
+    }
+
+    public void addToTop(List<Action> actionList){
+        actionQueue.AddRange(actionList); //I could just use the AddRange(action); whenever I need to
+    } // but doing this allows me to add a bunch more clarity on what's going on.
+
+    public void clearQueue(){
+        actionQueue.Clear(); //similar reasoning to above.
+    }
+
+    public Action nextAction(){
+        Action returnAction = actionQueue[0];
+        actionQueue.RemoveAt(0);
+        return returnAction;
     }
 }
--- Assets/Scripts/ui/damageTextController.cs
+++ Assets/Scripts/ui/damageTextController.cs
@@ -6,20 +6,23 @@ using TMPro;
 public class damageTextController : MonoBehaviour
 {
     public TMP_Text damageText;
-    [SerializeField] private float fadeLength = 1.0f;
+    [SerializeField] private float fadeLength = 2.0f;
     private float fadeStart;
     public Vector3 initialPositon, finalPosition;
-    public Color intialColor, finalColor; // 
+    public Color initialColor, finalColor; // 
     private float x;
     void Start(){
         fadeStart = Time.time;
-        Debug.Log(fadeStart);
     }
 
     void Update(){
         x = (Time.time-fadeStart)/fadeLength; //I don't know what to name this variable???
         if (x <=1){ //remember to report the crashes that were happening when you used a while lol
-            Debug.Log(x);
+            damageText.color = Color.Lerp(initialColor, finalColor, x);
+            transform.localPosition = Vector3.Lerp(initialPositon, finalPosition, x);
+        }
+        else{
+            Destroy(gameObject);
         }
     }
 }
